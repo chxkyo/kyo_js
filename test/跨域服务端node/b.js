978763f16eea2fc1c7618f3e8bd8c350433612f3
chx <you@example.com>
2016-10-17 19:15:49 +0800

    2016/10/17 19:15

---
+++ test/跨域服务端node/b.js
@@ -0,0 +1,20 @@
+var http = require("http");
+var url = require("url");
+var data =　{
+	'name':"kYo",
+	'age':'23'
+}
+http.createServer(function(req,res){
+	var params = url.parse(req.url,true);
+	console.log(params);
+	if(params.query){
+		if(params.query.userid == "007"){
+			if(params.query.callback){
+				var result = params.query.callback+"("+JSON.stringify(data)+")";
+				res.end(result);
+			}else{
+				res.end(JSON.stringify(data));
+			}
+		}
+	}
+}).listen(8888);
